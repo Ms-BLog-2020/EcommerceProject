7433fba25a63e1e9fc7c245555793aa31fb36260
sandy <a68b58@gmail.com>
2020-11-04 08:59:46 +0800

    載入Bootstrap套件

--- vuewebpack/src/App.vue
+++ vuewebpack/src/App.vue
@@ -17,19 +17,12 @@ export default {
     //所申請的APIPath
     console.log(process.env.APIPATH,process.env.CUSTOMPATH);
     this.$http.get(api).then((response) => {
-    console.log(response.data)
+    console.log(response.data);
     })
   },
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
 </style>
